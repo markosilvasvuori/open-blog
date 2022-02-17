fc240852d8133e2cc8ace2ddc9f401fa67b6ca9b
Marko Silvasvuori <Marko Silvasvuori>
2022-02-17 14:55:00 +0200

    Added Head

--- pages/new-post/index.js
+++ pages/new-post/index.js
@@ -1,14 +1,17 @@
 // domain.com/new-post
+
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-import NewPostForm from "../../src/components/Blog/NewPostForm";
+import PostForm from "../../src/components/Blog/PostForm";
 import styles from '../../styles/NewPostPage.module.css';
 
 const CreatePost = () => {
     const router = useRouter();
+
     const addNewPostHandler = async (enteredPostData) => {
-        const response = await fetch('/api/api', {
+        const response = await fetch('/api/api/', {
             method: 'POST',
             body: JSON.stringify(enteredPostData),
             headers: {'Content-Type': 'application/json'}
@@ -23,9 +26,13 @@ const CreatePost = () => {
 
     return (
         <Fragment>
+            <Head>
+                <title>Open Blog | New Post</title>
+                <meta name='description' content='Got a topic you would like to blog about? Go for it!' />
+            </Head>
             <h1 className={styles.title}>New Post</h1>
             <h2 className={styles['second-title']}>Got a topic you would like to blog about? Go for it!</h2>
-            <NewPostForm onAddNewPost={addNewPostHandler} />
+            <PostForm onAddNewPost={addNewPostHandler} />
         </Fragment>
     );
 };
