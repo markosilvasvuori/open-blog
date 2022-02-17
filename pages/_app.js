c67c4539d741fcdb59e7be19af3b192bf5aa193b
Marko Silvasvuori <Marko Silvasvuori>
2022-02-17 14:49:51 +0200

    Linked fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
+import Head from 'next/head';
 import Layout from '../src/components/Layout/Layout';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Open Blog | Blog Posts By Anyone</title>
+        <meta name="description" content="Blog posts by anyone" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital@0;1&family=Roboto:wght@700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
