253fdee4a71c9c95eccd2a1b6ba7bf104528d325
Marko Silvasvuori <Marko Silvasvuori>
2022-02-17 14:51:01 +0200

    Removed Head

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,23 @@
+// domain.com/about
+import { Fragment } from "react";
+import Head from "next/head";
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>
-                Got a topic you would like to blog about? Blog it! Don't like something you saw here? Delete it!
-                This is a blog where anyone is free to blog about whatever they like and delete what ever they want to delete.
-            </p>
-        </div>
+        <Fragment>
+            <Head>
+                <title>Open Blog | About</title>
+                <meta name='description' content='Open Blog is a blog where anyone is free to blog about whatever they like.' />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Got a topic you would like to blog about? Blog it! Don't like something you saw here? Delete it!
+                    Found a typo or want to contribute to an existing blog post? Edit it!
+                    This is a blog where anyone is free to manage the content!
+                </p>
+            </div>
+        </Fragment>
     );
 };
 
